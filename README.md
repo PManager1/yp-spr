10b697dcd17d77fca83dc254313d9d146fb1ae1d
jpca999 <jpca999@gmail.com>
2020-09-06 12:03:27 -0700

    in right direction feeding result of a into the b

--- README.md
+++ README.md
@@ -9,4 +9,5 @@ const bizName=$('.business-name').children('span').text()
 async await from here : 
 https://www.coreycleary.me/awaiting-multiple-requests-to-finish-using-promise-all/
 
-
+First solution
+https://stackoverflow.com/questions/28250680/how-do-i-access-previous-promise-results-in-a-then-chain
--- indexWithAsyncArray.js
+++ indexWithAsyncArray.js
@@ -30,6 +30,8 @@ const getCompanies = async () => {
     const companyName= bizName.substring(0, bizName.length - 10);
 
 
+    // console.log('before returning getCompanies');
+
     return {
       emailAddress,
       link,
@@ -52,6 +54,7 @@ const getStreetAddress = async () => {
     // console.log('15-stree-address =', e.children[0].parent.children[0].data );
     const streetAddress = e.children[0].parent.children[0].data;
 
+  // console.log('before returning getStreetAddress');
     return {
       streetAddress,
     }
@@ -62,86 +65,23 @@ const getStreetAddress = async () => {
 };
 
 
+// COMBINING ONE AFTER ANOTHER CALL 
 
-
-function getData() {
-  // the Fetch API returns a Promise
-  getCompanies()
-    .then((response) => {
-      console.log('69--first result =', response); 
-      // `.then()` is called after the request is complete
-      // this is part of the Fetch API for handling JSON-encoded responses
-      // return response.json();
-    })
-    .then((response) => {
-      // We can do whatever we want with the data now!
-      console.log('76--',response);
-    })
-
-    getStreetAddress()
-    .then((response) => {
-      console.log('81--result for getStreetAddress =', response); 
-      // `.then()` is called after the request is complete
-      // this is part of the Fetch API for handling JSON-encoded responses
-      // return response.json();
-    })
-
-
-    .then((response) => {
-      // We can do whatever we want with the data now!
-      console.log(response);
+function getExample() {
+    var a = getCompanies();
+    var b = a.then(function(resultA) {
+        // some processing
+        return getStreetAddress();
+    });
+    return Promise.all([a, b]).then(function([resultA, resultB]) {
+        // more processing
+        console.log(' resultB=', resultB);
+        console.log(' resultA=', resultA); 
+        return // something using both resultA and resultB
     });
 }
-getData();
 
-
-
-function runSerial() {
-    var that = this;
-    // task1 is a function that returns a promise (and immediately starts executing)
-    // task2 is a function that returns a promise (and immediately starts executing)
-    return Promise.resolve()
-        .then(function() {
-            return that.task1();
-        })
-        .then(function() {
-            return that.task2();
-        })
-        .then(function() {
-            console.log(" ---- done ----");
-        });
-}
-
-
-
-// userProfileData(); 
-
-
-
-// // CALLING EACH FUNCTIONS 
-// getCompanies()
-//   .then(result => {
-//     // result from first 
-//     console.log('first result =', result); 
-
-//     // combine the results here // add to the object. 
-//     //
-
-//     // const transformed = new otcsv(result);
-//     // return transformed.toDisk('./output4.csv');
-//   }). then(
-//   getStreetAddress()
-//   .then(result => {
-//     // result from first 
-//     console.log('getStreetAddress result =', result); 
-
-//     // combine the results here // add to the object. 
-
-//     const transformed = new otcsv(result);
-//     return transformed.toDisk('./output4.csv');
-//   }))
-//   .then(() => console.log('SUCCESSFULLY COMPLETED THE WEB SCRAPING SAMPLE'));
-
+getExample(); 
 
 
 
