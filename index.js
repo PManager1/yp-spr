1f2ec37db5da8b8dd6c3bdaf4aad914d9b830575
jpca999 <jpca999@gmail.com>
2020-09-09 23:10:35 -0700

    --sept-9

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ const otcsv = require('objects-to-csv');
 const cheerio = require('cheerio');
 
 
-
 const baseURL = 'https://www.yellowpages.com';
-// const searchURL = '/search?search_terms=Medical+Ambulance&geo_location_terms=San+Diego%2C+CA';
-const searchURL = '/search?search_terms=Medical%20Ambulance&geo_location_terms=marin%20county&page=2';
+const searchURL = '/search?search_terms=medical%20transport&geo_location_terms=Whittier%2C%20CA&page=5';
 
 // Function
 const getCompanies = async () => {
@@ -18,24 +16,24 @@ const getCompanies = async () => {
   const $ = cheerio.load(html);
 
   const businessMap = cheerio('a.business-name', html).map(async (i, e) => {
-    const streetAddress = $('div.result').eq(i).find('div.street-address').length == 1 ? $('div.result').eq(i).find('div.street-address').text() : '-';
+    const StreetAddress = $('div.result').eq(i).find('div.street-address').length == 1 ? $('div.result').eq(i).find('div.street-address').text() : '-';
     const City = $('div.result').eq(i).find('div.locality').length == 1 ? $('div.result').eq(i).find('div.locality').text() : '-';
     
     const link = baseURL + e.attribs.href;
     const innerHtml = await rp(link);
 
     const CompanyName = cheerio('h1', innerHtml).text();
-    const emailAddress = cheerio('a.email-business', innerHtml).prop('href');
+    const EmailAddress = cheerio('a.email-business', innerHtml).prop('href');
     const name = e.children[0].data;
     const Phone = cheerio('p.phone', innerHtml).text();
 
 
     return {
       CompanyName,
-      emailAddress,
+      EmailAddress,
       // link,
       Phone,
-      streetAddress,
+      StreetAddress,
       City
     }
   })
@@ -58,7 +56,7 @@ async function getExample() {
 getExample().then(result => {
 
     const transformed = new otcsv(result);
-    return transformed.toDisk('./testingonly.csv');
+    return transformed.toDisk('./Whittier_5.csv');
   })
   .then(() => console.log('SUCCESSFULLY COMPLETED THE WEB SCRAPING SAMPLE'));
 
